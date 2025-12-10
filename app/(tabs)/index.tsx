de74ddd2a7abc407349e3a8dec3332ebf571bfc7
PedroJGC <pedro.juliogc02@gmail.com>
2025-12-10 18:21:19 -0300

    feat: add Roboto font support and update expo-font dependency

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,22 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
+import { ActivityIndicator } from 'react-native'
 import { ThemeProvider } from 'styled-components'
 import { Groups } from '../components/Groups'
 import theme from '../theme'
 
 export default function Index() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   )
 }
