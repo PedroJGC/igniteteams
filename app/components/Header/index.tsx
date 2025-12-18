bb83f5b0ab617e4792707c8e8b439ddc476ac356
PedroJGC <pedro.juliogc02@gmail.com>
2025-12-18 19:09:50 -0300

    feat: enhance Header component to conditionally display BackButton with BackIcon

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,10 +1,19 @@
 import logoImg from '@/app/assets/logo.png'
-import { BackIcon, Container, Logo } from './styles'
+import { BackButton, BackIcon, Container, Logo } from './styles'
 
-export function Header() {
+type Props = {
+  showBackButton?: boolean
+}
+
+export function Header({ showBackButton = false }: Props) {
   return (
     <Container>
-      <BackIcon />
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
+
       <Logo source={logoImg} />
     </Container>
   )
--- app/components/Header/styles.ts
+++ app/components/Header/styles.ts
@@ -15,6 +15,11 @@ export const Logo = styled.Image`
   width: 46px;
   height: 55px;
 `
+
+export const BackButton = styled.TouchableOpacity`
+  flex: 1;
+`
+
 export const BackIcon = styled(CaretLeft).attrs(({ theme }: ThemeProps) => ({
   size: 32,
   color: theme.COLORS.WHITE,
